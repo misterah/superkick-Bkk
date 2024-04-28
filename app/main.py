9f68805310f8cacd328e0b24033e9fed5f2310fa
fah <arpapas.f@hotmail.com>
2024-04-28 18:15:30 +0700

    fix

--- app/main.py
+++ app/main.py
@@ -15,10 +15,10 @@ app.add_middleware(
 )
 
 app.include_router(products.router,  prefix="/api/products", tags=["products"])
-app.include_router(users_router, prefix="/api/users", tags=["Users"])
-app.include_router(orders_router, prefix="/api/orders", tags=["Orders"])
-app.include_router(dashboard_router, prefix="/api/dashboard", tags=["Dashboard"])
-app.include_router(shipping_router, prefix="/api/shipping", tags=["Shipping"])
+# app.include_router(users_router, prefix="/api/users", tags=["Users"])
+# app.include_router(orders_router, prefix="/api/orders", tags=["Orders"])
+# app.include_router(dashboard_router, prefix="/api/dashboard", tags=["Dashboard"])
+# app.include_router(shipping_router, prefix="/api/shipping", tags=["Shipping"])
 
 @app.get("/")
 async def hello():
