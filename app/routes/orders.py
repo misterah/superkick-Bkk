f83f12d3fb148c25c9c8b03d358e3a145118c6d3
fah <arpapas.f@hotmail.com>
2024-04-30 02:35:23 +0700

    add some admin's  crud about orders

--- app/routes/orders.py
+++ app/routes/orders.py
@@ -2,7 +2,11 @@ from fastapi import APIRouter, HTTPException
 from models.products import Product, Transaction, ShippingAddress
 from models.users import Customer
 from models.orders import *
-from config import db,products_collection, users_collection, orders_collection, counter_collection,shipping_collection
+from config import (db,products_collection,
+                    users_collection, 
+                    orders_collection, 
+                    counter_collection,
+                    shipping_collection)
 from bson import ObjectId, json_util
 import json
 
@@ -13,7 +17,6 @@ router = APIRouter()
 if "order_counter" not in db.list_collection_names():
     counter_collection().insert_one({"_id": "order_id", "value": 0})
 
-print("pass")
 
 @router.get("/")
 async def testorder():
@@ -24,16 +27,27 @@ async def testorder():
 @router.post("/checkout")
 def create_transaction(order_data: OrderData):
 
+    # ดึง username
     username_order = users_collection().find_one({"username":order_data.username})
+
+    # map order_data กับ ShippingAddress
     shipping_ad = ShippingAddress(username=username_order["username"],
                                     name=order_data.shipping_info.name,
                                     email=username_order["email"],
                                     postcode=order_data.shipping_info.postcode,
                                     tel=order_data.shipping_info.tel,
                                     address=order_data.shipping_info.address)
+    
+    # add to database
     add_shipping = shipping_collection().insert_one(shipping_ad.dict()).inserted_id
+
     total_price = 0
+
     all_products_order = [ ]
+
+    # loop products_order in order_data 
+    # for calculate price and make list for field in Transaction
+    # and use Transaction model to add data in orders_collection
     for products_order in order_data.products_order:
         product = products_collection().find_one({"sneakers_id": products_order.sneakers_id})
         if product:
@@ -47,28 +61,44 @@ def create_transaction(order_data: OrderData):
         else:
             raise HTTPException(status_code=404, detail=f"Product with ID {products_order.sneakers_id} not found")
         
-
+    #count order come in amount
     counter_collection().find_one_and_update(
         {"_id": "order_id"},
         {"$inc": {"value": 1}}
     )
-
      
-    # Get the current value of the counter (the new order_id)
+    # get the current value of the counter (the new order_id)
     order_id = counter_collection().find_one({"_id": "order_id"})["value"]
     order_id = str(order_id)
 
     shipping_complete = False
+
+    # map กับ Transaction model
     transaction = Transaction(order_id=order_id,
                               products=all_products_order,
                               total_price=total_price,
                               detail=shipping_ad,
                               complete=shipping_complete)
     
+    # add to database
     orders_collection().insert_one(transaction.dict()).inserted_id
     
-    # Return a response with the order_id
+    # Return a response
     return {"order": "Order created successfully", "order_detail": transaction}
 
-  
 
+@router.get("/all_orders")
+def admin_get_orders():
+    all_orders = orders_collection().find()
+    return {"all_orders": json.loads(json_util.dumps(all_orders))}
+
+
+@router.post("/add_order")
+def admin_add_order(order_data : Transaction):
+    
+    counter_collection().find_one_and_update(
+        {"_id": "order_id"},
+        {"$inc": {"value": 1}}
+    )
+    order = orders_collection().insert_one(order_data.dict()).inserted_id
+    return {"order": "Order created successfully", "order_id": str(order)}
